3afe214409b398a91a007620cd5b3fff72f5e370
Anton Kiselev <straw.berry.pie@yandex.ru>
2017-07-02 21:07:46 +0300

    Removed locale changes

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-locale-gen en_US.UTF-8 && update-locale && apt-get update -y && apt-get install -y bzip2 make build-essential curl libhdf5-dev
+apt-get update -y && apt-get install -y bzip2 make build-essential curl libhdf5-dev
+
+
 
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh >conda_install.sh && chmod +x conda_install.sh && ./conda_install.sh -b -p /usr/miniconda3 && rm conda_install.sh
 
